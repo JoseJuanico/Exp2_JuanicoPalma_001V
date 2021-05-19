6cc82fd13b26731dc7d44a48a3feeb0c2e2620ea
JoseJuanico <j.juanicopalma@gmail.com>
2021-05-18 23:48:56 -0400

    Se modifican todos los html, tambien se crea nuevo archivo llamado horario de atencion y se implementa api

--- js/funcionesJs.js
+++ js/funcionesJs.js
@@ -8,28 +8,27 @@ function calcularImc() {
 };
 
 function calculoEstadoFisico() {
-    var edad, imc, sexo, actividad  = "";
+    var edad, imc, actividad  = "";
     edad = document.getElementById("ledad").value;
-    sexo = document.getElementById("lgenero3").value;
     actividad = document.getElementById("selector").value;
     imc = calcularImc();
     if (imc < 18.5) {
-        alert("Genero: " + sexo + "\n" + "Imc: " + imc + "\n" + "Actividad: " + actividad + "\n" +
+        alert("Imc: " + imc + "\n" + "Actividad: " + actividad + "\n" +
         "Se encuentra en bajo peso"); 
     } else if (imc >= 18.5 && imc <= 24.9) {
-        alert("Genero: " + sexo + "\n" + "Imc: " + imc + "\n" +  "Actividad: " + actividad + "\n" +
+        alert("Imc: " + imc + "\n" +  "Actividad: " + actividad + "\n" +
         "Se encuentra en peso normal");
     } else if (imc > 24.9 && imc <= 29.9) {
-        alert("Genero: " + sexo + "\n" + "Imc: " + imc + "\n" +  "Actividad: " + actividad + "\n" +
+        alert("Imc: " + imc + "\n" +  "Actividad: " + actividad + "\n" +
         "Se encuentra en sobrepeso");
     } else if (imc >= 30 && imc <= 34.9) {
-        alert("Genero: " + sexo + "\n" + "Imc: " + imc + "\n" + "Actividad: " + actividad + "\n" +
+        alert("Imc: " + imc + "\n" + "Actividad: " + actividad + "\n" +
         "Se encuentra en obesidad tipo I");
     } else if (imc >= 35 && imc <= 39.9) {
-        alert("Genero: " + sexo + "\n" + "Imc: " + imc + "\n" + "Actividad: " + actividad + "\n" +
+        alert("Imc: " + imc + "\n" + "Actividad: " + actividad + "\n" +
         "Se encuentra en obesidad tipo II");
     } else {
-        alert("Genero: " + sexo + "\n" + "Imc: " + imc + "\n" + "Actividad: " + actividad + "\n" + 
+        alert("Imc: " + imc + "\n" + "Actividad: " + actividad + "\n" + 
         "Se encuentra en obesidad tipo III");
     }
 };
